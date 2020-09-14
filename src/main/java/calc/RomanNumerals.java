3e95ae083e59f09d39d7bd8633f7c85a5efa08e8
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2020-09-14 17:39:14 +0300

    Тестовое задание калькулятор Java (исправление2)

--- src/main/java/calc/RomanNumerals.java
+++ src/main/java/calc/RomanNumerals.java
@@ -23,11 +23,7 @@ public class RomanNumerals {
             put("VIII", 8);
             put("IX", 9);
             put("X", 10);
-            put("C", 100);
-            put("XC", 90);
-            put("L", 50);
-            put("XL", 40);
-            }
+        }
     };
 
     public double maping(){
@@ -45,11 +41,43 @@ public class RomanNumerals {
     }
 
     public String intToRomanNumerals() {
+        String resalt = "";
+        int inputInt = Integer.valueOf(this.input);
+        if (inputInt == 100) {
+            resalt = "C";
+        } else if (inputInt >= 90) {
+            resalt = "XC" + parseRomanNumerals(inputInt - 90);
+        } else if (inputInt >= 80) {
+            resalt = "LXXX" + parseRomanNumerals(inputInt - 80);
+        } else if (inputInt >= 70) {
+            resalt = "LXX" + parseRomanNumerals(inputInt - 70);
+        } else if (inputInt >= 60) {
+            resalt = "LX" + parseRomanNumerals(inputInt - 60);
+        } else if (inputInt >= 50) {
+            resalt = "L" + parseRomanNumerals(inputInt - 50);
+        } else if (inputInt >= 40) {
+            resalt = "XL" + parseRomanNumerals(inputInt - 40);
+        } else if (inputInt >= 30) {
+            resalt = "XXX" + parseRomanNumerals(inputInt - 30);
+        } else if (inputInt >= 20) {
+            resalt = "XX" + parseRomanNumerals(inputInt - 20);
+        } else if (inputInt >= 10) {
+            resalt = "X" + parseRomanNumerals(inputInt - 10);
+        } else if (inputInt >= 1) {
+            resalt = parseRomanNumerals(inputInt);
+        } else {
+            throw new IllegalArgumentException("Результат " + inputInt + " не можут быть записан Римскими цифрами");
+        }
+        return resalt;
+    }
+
+    public String parseRomanNumerals(int input) {
         String resaltStr = "";
         for (Map.Entry<String, Integer> map : INT_MAPER.entrySet()
-             ) {
-            if (Integer.parseInt(this.input) == map.getValue()) {
+        ) {
+            if (input == map.getValue()) {
                 resaltStr = map.getKey();
+                break;
             }
         }
         return resaltStr;
